584ce841555d868d3b78327529f56ef97fd91445
Wes Doyle <wesdoyle08@gmail.com>
2018-11-07 19:29:24 -0600

    Pipelines coverage, add score_sentiment to pipeline

--- pipelines.py
+++ pipelines.py
@@ -1,4 +1,5 @@
 from tokenizers import SimpleSentenceTokenizer
+from sentiment_analyzer import SimpleSentimentAnalyzer
 
 class SimplePipeline(object):
     """
@@ -6,7 +7,7 @@ class SimplePipeline(object):
     """
     def __init__(self, raw_text, features):
         """
-        :param: raw_text: string raw text document containing one or more sentences
+        :param: input_text: string raw text document containing one or more sentences
         :param: features: list<string> features to run in pipeline
 
         """
@@ -14,14 +15,51 @@ class SimplePipeline(object):
         self.features = features
 
     def run(self):
+        """
+        Execute the transformation methods on attrs in the pipeline
+        """
         try:
+            res = self.raw_text
             for feature in self.features:
-                getattr(self, feature)(self.raw_text)
+                res = getattr(self, feature)(res)
 
         except AttributeError as e:
             print("SimplePipeline supports no feature named: {}"
                     .format(feature))
 
-    def sent_tokenize(self, raw_text):
+    def sent_tokenize(self, input_text):
+        """
+        Invokes the tokenize method on SimpleSentenceTokenizer
+        :param input_text: string text to tokenize
+        """
         st = SimpleSentenceTokenizer()
-        st.tokenize(raw_text)
+        return st.tokenize(input_text)
+
+    def word_tokenize(self, input_text):
+        """
+        Invokes the tokenize method on SimpleWordTokenizer
+        :param input_text: string text to tokenize
+        """
+        st = SimpleTokenizer()
+        return st.tokenize(input_text)
+
+    def score_sentiment(self, input_text):
+        """
+        Invokes the score method on the SimpleSentimentAnalyzer
+        :param input_text: string text to sentiment score
+        """
+        sa = SimpleSentimentAnalyzer()
+        return sa.score(input_text)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tests/test_simple_pipeline.py
+++ tests/test_simple_pipeline.py
@@ -7,11 +7,53 @@ class TestSimplePipeline:
 
     @patch('pipelines.SimplePipeline.sent_tokenize')
     def test_simple_pipeline_should_invoke_sentence_tokenizer(self, mock_st):
-        raw_text = "foo! bar baz, and. quux?"
+        input_text = "foo! bar baz, and. quux?"
         features = ['sent_tokenize']
-        sut = SimplePipeline(raw_text, features)
+        sut = SimplePipeline(input_text, features)
         sut.run()
-        sut.sent_tokenize.assert_called_once_with(raw_text)
+        sut.sent_tokenize.assert_called_once_with(input_text)
+
+    @patch('pipelines.SimplePipeline.word_tokenize')
+    def test_simple_pipeline_should_invoke_word_tokenizer(self, mock_wt):
+        input_text = "foo! bar baz, and. quux?"
+        features = ['word_tokenize']
+        sut = SimplePipeline(input_text, features)
+        sut.run()
+        sut.word_tokenize.assert_called_once_with(input_text)
+
+    @patch('pipelines.SimplePipeline.score_sentiment')
+    def test_simple_pipeline_should_invoke_score_sentiment(self, mock_ss):
+        input_text = "Good stuff"
+        features = ['score_sentiment']
+        sut = SimplePipeline(input_text, features)
+        sut.run()
+        sut.score_sentiment.assert_called_once_with(input_text)
+
+    @patch('pipelines.SimplePipeline.word_tokenize')
+    @patch('pipelines.SimplePipeline.sent_tokenize')
+    def test_simple_pipeline_should_invoke_two_features_in_order(self, mock_st, mock_wt):
+        mock_st.return_value = "foo"
+        input_text = "foo! bar baz, and. quux?"
+        features = ['sent_tokenize', 'word_tokenize']
+        sut = SimplePipeline(input_text, features)
+        sut.run()
+        sut.sent_tokenize.assert_called_once_with(input_text)
+        sut.word_tokenize.assert_called_once_with("foo")
+
+    @patch('pipelines.SimplePipeline.score_sentiment')
+    @patch('pipelines.SimplePipeline.word_tokenize')
+    @patch('pipelines.SimplePipeline.sent_tokenize')
+    def test_simple_pipeline_should_invoke_three_features_in_order(self, mock_st, mock_wt, mock_ss):
+        mock_st.return_value = "foo"
+        mock_wt.return_value = 1234
+        input_text = "foo! bar baz, and. quux?"
+        features = ['sent_tokenize', 'word_tokenize', 'score_sentiment']
+        sut = SimplePipeline(input_text, features)
+        sut.run()
+        sut.sent_tokenize.assert_called_once_with(input_text)
+        sut.word_tokenize.assert_called_once_with("foo")
+        sut.score_sentiment.assert_called_once_with(1234)
+
 
     @pytest.mark.parametrize("input",
             [
@@ -19,11 +61,11 @@ class TestSimplePipeline:
             ]
         )
     def test_simple_pipeline_should_invoke_sentence_tokenizer(self, input):
-        raw_text = "foo! bar baz, and. quux?"
+        input_text = "foo! bar baz, and. quux?"
         features = ['foo']
-        sut = SimplePipeline(raw_text, features)
+        sut = SimplePipeline(input_text, features)
 
         with pytest.raises(AttributeError):
             sut.run()
-            sut.sent_tokenize.assert_called_once_with(raw_text)
+            sut.sent_tokenize.assert_called_once_with(input_text)
 
