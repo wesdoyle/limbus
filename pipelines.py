d41d8e2bcfa4367b696f4196d37de45b673ca235
Wes Doyle <wesdoyle08@gmail.com>
2018-11-07 19:07:13 -0600

    Add pipeline tests for run method

--- pipelines.py
+++ pipelines.py
@@ -1,1 +1,27 @@
+from tokenizers import SimpleSentenceTokenizer
 
+class SimplePipeline(object):
+    """
+    Provides a means for linking NLP classes together that transform text data
+    """
+    def __init__(self, raw_text, features):
+        """
+        :param: raw_text: string raw text document containing one or more sentences
+        :param: features: list<string> features to run in pipeline
+
+        """
+        self.raw_text = raw_text
+        self.features = features
+
+    def run(self):
+        try:
+            for feature in self.features:
+                getattr(self, feature)(self.raw_text)
+
+        except AttributeError as e:
+            print("SimplePipeline supports no feature named: {}"
+                    .format(feature))
+
+    def sent_tokenize(self, raw_text):
+        st = SimpleSentenceTokenizer()
+        st.tokenize(raw_text)
--- tests/test_simple_pipeline.py
+++ tests/test_simple_pipeline.py
@@ -1,14 +1,29 @@
 from mock import patch
+import pytest
+
+from pipelines import SimplePipeline
 
 class TestSimplePipeline:
 
-    @patch('pipelines.SimplePipeline.__new__')
-    def test_simple_pipeline_should_invoke_provided_features_for_text(self, mock_pipeline):
+    @patch('pipelines.SimplePipeline.sent_tokenize')
+    def test_simple_pipeline_should_invoke_sentence_tokenizer(self, mock_st):
         raw_text = "foo! bar baz, and. quux?"
-        features = ['bang']
+        features = ['sent_tokenize']
         sut = SimplePipeline(raw_text, features)
-        nlp_pipeline.run()
-        sut.bang.assert_called_once_with(raw_text)
+        sut.run()
+        sut.sent_tokenize.assert_called_once_with(raw_text)
 
+    @pytest.mark.parametrize("input",
+            [
+                (['foo']), (['foo', 'sent_tokenize'], ['foo', 'bar'])
+            ]
+        )
+    def test_simple_pipeline_should_invoke_sentence_tokenizer(self, input):
+        raw_text = "foo! bar baz, and. quux?"
+        features = ['foo']
+        sut = SimplePipeline(raw_text, features)
 
+        with pytest.raises(AttributeError):
+            sut.run()
+            sut.sent_tokenize.assert_called_once_with(raw_text)
 
