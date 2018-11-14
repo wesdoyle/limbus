10416492be5873f14b6d052cfff29019feaac4db
Wes Doyle <wesdoyle08@gmail.com>
2018-11-14 08:22:49 -0600

    Update setup

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name="Simple Sentiment Analyzer", version="0.0.1", packages=find_packages())
+setup(name="bluebook", version="0.0.1", packages=find_packages())
