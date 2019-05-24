f56f85b96a3fea68e972a3eb4e8230bb3ce6c6f2
wesdoyle <wesdoyle08@gmail.com>
2019-05-23 21:51:15 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(name="bluebook", version="0.0.1", packages=find_packages())
+
+from distutils.core import setup
+setup(
+        name = 'bluebook',
+        packages = ['bluebook'],
+        version = '0.0.1',
+        license='Apache',
+        description = 'A library for simple text-based natural language processing tasks',
+        author = 'Wes Doyle',
+        url = 'https://github.com/user/wesdoyle',
+        keywords = ['nlp', 'natural-language-processing', 'sentiment-analysis'],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+            ],
+        )
